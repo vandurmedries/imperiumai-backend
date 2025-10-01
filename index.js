309d2730c814e75311e4ccb6ef3f8c4a760515d9
vandurmedries <108144422+vandurmedries@users.noreply.github.com>
2025-10-01 04:29:32 +0200

    Update index.js

--- index.js
+++ index.js
@@ -9,3 +9,10 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+// Voeg dit toe aan je backend (index.js of app.js):
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
